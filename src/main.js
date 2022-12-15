8889b7c5f6081c7c6d550c77a6f28c4ab5e1e8a5
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2022-12-15 17:04:53 +0000

    zet
    at

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './stores'
-
-createApp(App).use(store).use(router).mount('#app')
+import{createPinia} from 'pinia'
+createApp(App).use(store).use(createPinia()).use(router).mount('#app')
